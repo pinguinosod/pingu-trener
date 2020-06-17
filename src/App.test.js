315197636f26e3cdb480bfd512e1eb90bce67e6f
PinguinoSod <pinguino.sod@gmail.com>
2020-06-18 01:15:50 +0200

    Create unit tests for App and NavigationBar

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,36 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
+import Input from './components/Input';
+import Result from './components/Result';
+import UserConfiguration from './components/UserConfiguration';
+import NavigationBar from './components/NavigationBar';
 
-test('renders Pingu Trener title', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/Pingu Trener/i);
-  expect(linkElement).toBeInTheDocument();
+configure({adapter : new Adapter()});
+
+describe('App', () => {
+
+  it('shoud render header, main and footer', () => {
+    const wrapper = shallow(<App />);
+
+    expect(wrapper.find('header')).toHaveLength(1);
+    expect(wrapper.find('main')).toHaveLength(1);
+    expect(wrapper.find('footer')).toHaveLength(1);
+  });
+
+  it('shoud render Pingu Trener title', () => {
+    const wrapper = shallow(<App />);
+
+    expect(wrapper.find('header > h1').text()).toBe('Pingu Trener');
+  });
+
+  it('shoud render Input, Result, UserConfiguration and NavigationBar', () => {
+    const wrapper = shallow(<App />);
+
+    expect(wrapper.find('main').find(Input)).toHaveLength(1);
+    expect(wrapper.find('main').find(Result)).toHaveLength(1);
+    expect(wrapper.find('main').find(UserConfiguration)).toHaveLength(1);
+    expect(wrapper.find('footer').find(NavigationBar)).toHaveLength(1);
+  });
 });
---
+++ src/components/NavigationBar.test.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import NavigationBar from './NavigationBar';
+
+configure({adapter : new Adapter()});
+
+describe('App', () => {
+  
+
+  it('shoud render 2 buttons, first with text Viaje and second with text Mis Datos', () => {
+    const wrapper = shallow(<NavigationBar />);
+
+    expect(wrapper.find('button')).toHaveLength(2);
+    expect(wrapper.find('button:first-child').text()).toBe('Viaje');
+    expect(wrapper.find('button:last-child').text()).toBe('Mis Datos');
+  });  
+
+  it('shoud give .active class to first button when selected tab is viaje', () => {
+    const wrapper = shallow(<NavigationBar selectedTab={'viaje'} />);
+
+    expect(wrapper.find('button:first-child').hasClass('active')).toBeTruthy();
+    expect(wrapper.find('button:last-child').hasClass('active')).toBeFalsy();
+  });  
+
+  it('shoud give .active class to second button when selected tab is datos', () => {
+    const wrapper = shallow(<NavigationBar selectedTab={'datos'} />);
+
+    expect(wrapper.find('button:first-child').hasClass('active')).toBeFalsy();
+    expect(wrapper.find('button:last-child').hasClass('active')).toBeTruthy();
+  });
+
+  it('shoud call changeTab method with viaje as parameter when first button is clicked', () => {
+    const mockChangeTab = jest.fn();
+    const wrapper = shallow(<NavigationBar changeTab={mockChangeTab} />);
+    const firstButton = wrapper.find('button:first-child');
+
+    firstButton.simulate('click');
+
+    expect(mockChangeTab).toHaveBeenCalledWith('viaje');
+  });
+
+  it('shoud call changeTab method with datos as parameter when second button is clicked', () => {
+    const mockChangeTab = jest.fn();
+    const wrapper = shallow(<NavigationBar changeTab={mockChangeTab} />);
+    const lastButton = wrapper.find('button:last-child');
+
+    lastButton.simulate('click');
+
+    expect(mockChangeTab).toHaveBeenCalledWith('datos');
+  });
+});
